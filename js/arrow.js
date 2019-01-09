6d462718060aed3946eaefe8515273af844cbbed
Patryk <hillj1089@gmail.com>
2019-01-09 18:22:02 +0100

    Arrow-bottom added

---
+++ js/arrow.js
@@ -0,0 +1,7 @@
+const arrowDiv = document.querySelector('.arrowPush');
+
+const arrowClick = () => {  
+    const pushBar = window.scrollTo(0, 0);
+}
+
+arrowDiv.addEventListener('click', arrowClick);
